45cee4648d56a429283ad706c07c7eeb9fc50a28
Huynh Tien Tai <taiht221@gmail.com>
2022-01-22 22:06:40 +0700

    New change

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log(1)
+import axiosClient from './api/axiosClient'
+import postApi from './api/postApi'
+
+async function main() {
+  const response = await axiosClient.get('/posts')
+  console.log(response)
+
+  const params = {
+    _page: 1,
+    _limit: 5,
+  }
+  const reo = await postApi.getAll(params)
+  console.log(reo)
+}
+
+main()
