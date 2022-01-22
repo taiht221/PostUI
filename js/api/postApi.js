ae1f9bde0fcfb6cad558fcb144a980ac0f544637
Huynh Tien Tai <taiht221@gmail.com>
2022-01-22 11:05:53 +0700

    save change

---
+++ js/api/postApi.js
@@ -0,0 +1,29 @@
+import axiosClient from './axiosClient'
+
+const postApi = {
+  getAll(params) {
+    const url = '/posts'
+    return axiosClient.get(url, { params: params })
+  },
+
+  getById(id) {
+    const url = `/posts/${id}`
+    return axiosClient.get(url)
+  },
+
+  add(data) {
+    const url = '/posts'
+    return axiosClient.patch(url, data)
+  },
+
+  update(data) {
+    const url = `/posts/${data.id}`
+    return axiosClient.patch(url, data)
+  },
+
+  delete(id) {
+    const url = `/posts/${id}`
+    return axiosClient.patch(url)
+  },
+}
+export default postApi
