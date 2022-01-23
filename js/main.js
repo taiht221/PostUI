3dc6665cd3e665d5b12bbc3325a0c80eed617c05
Huynh Tien Tai <taiht221@gmail.com>
2022-01-23 16:38:37 +0700

    New change
    -axios interceptor

--- js/main.js
+++ js/main.js
@@ -3,14 +3,12 @@ import postApi from './api/postApi'
 
 async function main() {
   const response = await axiosClient.get('/posts')
-  console.log(response)
 
   const params = {
     _page: 1,
     _limit: 5,
   }
   const reo = await postApi.getAll(params)
-  console.log(reo)
 }
 
 main()
