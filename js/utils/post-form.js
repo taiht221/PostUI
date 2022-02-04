919820a05b1d9b354a4e7ef3968a527e271f66c1
Huynh Tien Tai <taiht221@gmail.com>
2022-02-04 16:20:54 +0700

    Validate form with yup

--- js/utils/post-form.js
+++ js/utils/post-form.js
@@ -1,8 +1,8 @@
-import { setBackgroundImg } from '.'
+import { setBackgroundImg, setTextContent } from '.'
 import { setFieldValues } from './common'
+import * as yup from 'yup'
 
 function setFormValues(form, formValues) {
-  console.log(formValues)
   setFieldValues(form, "[name='title']", formValues?.title)
   setFieldValues(form, "[name='author']", formValues?.author)
   setFieldValues(form, "[name='description']", formValues?.description)
@@ -10,10 +10,98 @@ function setFormValues(form, formValues) {
   setBackgroundImg(document, '#postHeroImage', formValues?.imageUrl)
 }
 
+function getFormValues(form) {
+  const values = {}
+
+  //   ;['title', 'author', 'description', 'imageUrl'].forEach((name) => {
+  //     const field = form.querySelector(`[name='${name}']`)
+  //     if (field) values[name] = field.value
+  //   })
+
+  const data = new FormData(form)
+  for (const [key, value] of data) {
+    values[key] = value
+  }
+  return values
+}
+
+function createYupSchema() {
+  return yup.object().shape({
+    title: yup.string().required('Please enter title'),
+    author: yup
+      .string()
+      .required('Please enter author')
+      .test(
+        'at-least-two-word',
+        'Please enter at least two word',
+        (value) => value.split(' ').filter((x) => !!x && x.length >= 3).length >= 2
+      ),
+    description: yup.string(),
+  })
+}
+
+function setFieldError(form, name, error) {
+  const element = form.querySelector(`[name="${name}"]`)
+  if (element) {
+    element.setCustomValidity(error)
+    setTextContent(element.parentElement, '.invalid-feedback', error)
+  }
+}
+
+async function validateForm(form, formValues) {
+  // get errors --- set errors --- add was-validated class bs
+  const errors = {
+    title: createYupSchema,
+  }
+
+  //set errors
+
+  for (const key in errors) {
+    const element = form.querySelector(`[name="${key}"]`)
+    if (element) {
+      element.setCustomValidity(errors[key])
+      setTextContent(element.parentElement, '.invalid-feedback', errors[key])
+    }
+  }
+
+  try {
+    // reset previous errors
+    ;['title', 'author'].forEach((name) => setFieldError(form, name, ''))
+
+    const schema = createYupSchema()
+    await schema.validate(formValues, { abortEarly: false })
+  } catch (error) {
+    const errorLog = {}
+
+    if (error.name === 'ValidationError') {
+      for (const validationError of error.inner) {
+        const name = validationError.path
+
+        if (errorLog[name]) continue
+
+        setFieldError(form, name, validationError.message)
+        errorLog[name] = true
+      }
+    }
+  }
+
+  // add was-validated class to form Element
+  const isValid = form.checkValidity()
+  if (!isValid) form.classList.add('was-validated')
+  return isValid
+}
+
 export function initPostForm({ formId, defaultValues, onSubmit }) {
   const form = document.getElementById(formId)
-  console.log(form)
   if (!form) return
 
   setFormValues(form, defaultValues)
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const formValues = getFormValues(form)
+
+    if (!validateForm(form, formValues)) return
+  })
 }
